4f1757e5d43547d75f8711863fff52c8354094c6
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2025-05-22 16:47:51 +0300

    save local pc commits

--- board/__init__.py
+++ board/__init__.py
@@ -3,6 +3,5 @@ from board import pages
 
 def create_app():
     app = Flask(__name__)
-
     app.register_blueprint(pages.bp)
     return app
--- board/pages.py
+++ board/pages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 bp = Blueprint("pages", __name__)
 
@@ -14,3 +14,6 @@ def about():
     # return "<a href='/'>  <h1 style='color:purple'>  Hello, About! </h1> </a>"
     return render_template("pages/about.html") 
     
+@bp.route("/printer")
+def printer():
+    return redirect("http://192.168.1.11:631", code=302) #using nmap tool (nmap -sS <printer-local-ip>) we tought that my printer uses ipp protocol on port 631/tcp 
