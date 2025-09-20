87e214b88ca743d7f1f1ad15a1ae6ea791628e4e
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2025-09-20 18:40:29 +0300

    new update is out `v1.0.1B` print from anywhere beta feature is out com new update is out `v1.0.1B` print from anywhere beta feature is out

--- board/__init__.py
+++ board/__init__.py
@@ -3,7 +3,7 @@
 # Version: 1.0.0-Beta
 # Author: Omar Rashad
 # Python Version: 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)]
-# Last Update: 2025-05-22
+# Last Update: 2025-09-20
 # =============================================
 from flask import Flask
 from board import pages
--- board/printing.py
+++ board/printing.py
@@ -1,6 +1,16 @@
-
+# =============================================
+# File: printing.py
+# Version: 1.0.1-Beta
+# Author: Omar Rashad
+# Python Version: 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)]
+# Last Update: 2025-05-22
+# =============================================
 import cups
 import os
+
+import time
+
+IPP_JOB_COMPLETED = 9
 
 def print_file(file_path: str) -> tuple[bool, str]:
     """
@@ -21,9 +31,10 @@ def print_file(file_path: str) -> tuple[bool, str]:
 
         printer_name = list(printers.keys())[0]
 
-        
-
         job_id = conn.printFile(printer_name, file_path, "Web Print Job", {})
+
+        while conn.getJobAttributes(job_id)['job-state'] != IPP_JOB_COMPLETED:
+            time.sleep(1)
 
         message = f"Job sent successfully. Job ID: {job_id}"
         print(message)
